36949da9e49559b330744948a788a703aace4cc6
Renner Borges <rennerferreira23@gmail.com>
2022-04-08 08:42:21 -0300

    feat: adicionando tratamento de erros

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './src/routes';
+import errorController from './src/controllers/error';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(router);
 
-app.use(function onError(err, req, res, next) {
-  res.status(400).json({ error: err.message });
-});
+app.use(errorController.setError404);
+app.use(errorController.errorSend);
+
 export default app;
