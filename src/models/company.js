50f291c9042a68380169efe96c9f0adedc0a51eb
Renner Borges <rennerferreira23@gmail.com>
2022-05-10 08:37:50 -0300

    refactor: corrigindo identação do código

--- src/models/company.js
+++ src/models/company.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const modelSchema = new mongoose.Schema({
-    fantasyName: String,
-    corporateName: String,
-    cnpj: String,
-    areaOfOperation: String,
-    enabled: Boolean
+  fantasyName: String,
+  corporateName: String,
+  cnpj: String,
+  areaOfOperation: String,
+  enabled: Boolean,
 });
 
 const modelName = 'companies';
 
-if(mongoose.connection && mongoose.connection.models[modelName]){
-    module.exports = mongoose.connection.models[modelName];//Conexão
-}else{
-    module.exports = mongoose.model(modelName,modelSchema);//Criar nova conexão
+if (mongoose.connection && mongoose.connection.models[modelName]) {
+  module.exports = mongoose.connection.models[modelName]; //Conexão
+} else {
+  module.exports = mongoose.model(modelName, modelSchema); //Criar nova conexão
 }
--- src/models/point.js
+++ src/models/point.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const modelSchema = new mongoose.Schema({
-    date: Date,
-    user: String,
-    company: String,
-    enabled: Boolean
+  date: Date,
+  user: String,
+  company: String,
+  enabled: Boolean,
 });
 
 const modelName = 'points';
 
-if(mongoose.connection && mongoose.connection.models[modelName]){
-    module.exports = mongoose.connection.models[modelName];//Conexão
-}else{
-    module.exports = mongoose.model(modelName,modelSchema);//Criar nova conexão
+if (mongoose.connection && mongoose.connection.models[modelName]) {
+  module.exports = mongoose.connection.models[modelName]; //Conexão
+} else {
+  module.exports = mongoose.model(modelName, modelSchema); //Criar nova conexão
 }
