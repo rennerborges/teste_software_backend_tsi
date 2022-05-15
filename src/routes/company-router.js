76fce1c8d439717ad218c2ae89c11bbc3fa5dfb0
Renner Borges <rennerferreira23@gmail.com>
2022-05-14 22:32:33 -0300

    feat: criando validação de body para a rota de login

--- src/routes/company-router.js
+++ src/routes/company-router.js
@@ -7,10 +7,11 @@ import { Auth } from '../middleware/auth-middleware';
 import ValidationEnvironmentPost from '../validation/company-post-validation';
 import ValidationCompanyEdit from '../validation/company-edit-validation';
 import companyMiddleware from '../middleware/company-middleware';
+import ValidatorLogin from '../validation/login-validation';
 
 const router = express.Router();
 
-router.post('/login', authController.login);
+router.post('/login', ValidatorLogin, authController.login);
 
 router.get('/company', Auth('g'), companyController.getEnvironments);
 
